6f40e9f2dcc3fdf07d84daab26a4f05995f0d895
soyonoyoso <y9235234@gmail.com>
2026-01-14 17:46:11 +0900

    feat(battle): 전투 시스템 및 플레이어 턴 메서드 추가

---
+++ Systems/BattleSystem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using TextRPG.Models;
+namespace TextRPG.Systems;
+
+public class BattleSystem
+{
+    #region 던전 입장 - 전투 실행
+    // 전투 시작 메서드
+    // 반환값 : 전투 승리 여부
+    public bool StartBattle(Player player, Enemy enemy)
+    {
+        Console.Clear();
+        
+    }
+    #endregion
+}
